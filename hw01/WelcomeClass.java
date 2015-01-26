f02507331d21ed6073dc5ccf54e8407792883aa8
macmcguire <pmm215@lehigh.edu>
2015-01-26 01:06:29 +0000

    Updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+// Patrick McGuire 
+// Demo 0
+// WelcomeClass Java program 
+//
+//  first compile the program 
+//      javac WelcomeClass.Java
+//  run the program 
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+// add main method 
+    public static void main(String[] args) {
+        
+// print "WELCOME" with box around it, then my student number in between a unique design and then end with a short bio 
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    System.out.println(" ^ ^ ^ ^ ^ ^ ");
+    System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("<-P--M--M--2--1--5->");
+    System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println(" v  v  v  v  v  v");
+    System.out.println("I studied abroad the spring semester of my Junior year. I attended University of Cape Town");
+    
+    //test/testtest
+    
+    }
+}
